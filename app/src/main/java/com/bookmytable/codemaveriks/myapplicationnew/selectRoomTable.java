6e4eda2e18c734efba642fcf3f64e3d81aa2a383
Ace Manual <mratapattu.15@cse.mrt.ac.lk>
2018-03-01 12:15:05 +0530

    BookMyTable mobile app

---
+++ app/src/main/java/com/bookmytable/codemaveriks/myapplicationnew/selectRoomTable.java
@@ -0,0 +1,14 @@
+package com.bookmytable.codemaveriks.myapplicationnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selectRoomTable extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_room_table);
+    }
+
+}
--- app/src/main/java/com/bookmytable/codemaveriks/myapplicationnew/select_room_table.java
+++
@@ -1,5 +0,0 @@
-package com.bookmytable.codemaveriks.myapplicationnew;
-
-
-public class select_room_table {
-}
